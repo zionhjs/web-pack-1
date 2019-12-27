c59d80d005845b1eeb1eefd444329f55035b363d
zionHuang <hjszion@gmail.com>
2019-12-27 11:49:51 -0800

    update resume

--- GoogleDrive_Folder/Code_Practices/React_Projects/portfolio_proj/src/components/resume.js
+++ GoogleDrive_Folder/Code_Practices/React_Projects/portfolio_proj/src/components/resume.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Resume extends Component {
     render() {
         return (
             <div>
-                <h1>Resume Page</h1>
-            </div>
+                <Grid>
+                    <Cell col={4}>
+                        <div style={{ textalign: 'center' }}>
+                            <img
+                                src="https://www.zivarentals.com/wp-content/uploads/2017/09/avatar-1606916_960_720-1.png"
+                                alt="avatar"
+                                style={{ height: '200px' }}
+                            />
+                        </div>
+                    </Cell>
+                    <Cell className="resume-right-col" col={8}>Right Side</Cell>
+                </Grid>
+            </div >
         )
     }
 }
