ce77da47ba6f65b2f47770765684977f6c993df1
zionHuang <hjszion@gmail.com>
2020-01-05 18:40:33 -0800

    update css

--- GoogleDrive_Folder/Code_Practices/React_Projects/portfolio_proj/src/js/nav-toggle.js
+++ GoogleDrive_Folder/Code_Practices/React_Projects/portfolio_proj/src/js/nav-toggle.js
@@ -3,12 +3,22 @@
         var nav_icon = document.getElementById('nav-icon');
         nav_icon.addEventListener('click', navClickHandler(e, nav_icon), false);
     }
-
-    function navClickHandler(e, obj) {
-        console.log('1111');
-        obj.children[0].classList.toggle('nav-line-actived-1');
-        obj.children[2].classList.toggle('nav-line-actived-2');
-        obj.children[2].classList.toggle('nav-line-actived-3');
-    }
 })()
+
+function navClickHandler(e, obj) {
+    console.log('1111');
+    obj.children[0].classList.toggle('nav-line-actived-1');
+    obj.children[1].classList.toggle('nav-line-actived-2');
+    obj.children[2].classList.toggle('nav-line-actived-3');
+    obj.children[0].classList.toggle('nav-line');
+    obj.children[1].classList.toggle('nav-line');
+    obj.children[2].classList.toggle('nav-line');
+
+    var card = document.getElementsByClassName('nav-card');
+    card[0].classList.toggle('card-child-1');
+    card[1].classList.toggle('card-child-2');
+    card[2].classList.toggle('card-child-3');
+    card[3].classList.toggle('card-child-4');
+    card[4].classList.toggle('card-child-5');
+}
 
